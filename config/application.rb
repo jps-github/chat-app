6bdc7137dee01957814933a91cdc2ac2f0ac3c66
jps-github <jumpei.suzuki@di-v.co.jp>
2021-04-28 18:02:00 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,11 @@ module ChatApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    #config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.test_framework false
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
