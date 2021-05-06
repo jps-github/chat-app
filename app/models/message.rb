df960fbef5d16b2226d9bdd9c1886c35be3d61ca
jps-github <jumpei.suzuki@di-v.co.jp>
2021-05-06 18:03:52 +0900

    エラー

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,11 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
+<<<<<<< Updated upstream
 
   validates :content, presence: true, unless: :was_attached?
+=======
+>>>>>>> Stashed changes
 
   def was_attached?
     self.image.attached?
