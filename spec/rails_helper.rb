c21e6336cc52e315c90a04ed6351b0f92fec365c
jps-github <jumpei.suzuki@di-v.co.jp>
2021-05-07 10:20:09 +0900

    各モデルのテスト

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,10 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-<<<<<<< Updated upstream
-=======
 I18n.locale = "en"
->>>>>>> Stashed changes
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
